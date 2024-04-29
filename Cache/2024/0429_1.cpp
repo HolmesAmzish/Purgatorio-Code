75df59bae6035298fd3169b91252efbc0d472493
Nulla <holmesamzish86@outlook.com>
2024-04-29 09:02:16 +0800

    20240429:0902

--- Cache/2024/0429_1.cpp
+++ Cache/2024/0429_1.cpp
@@ -31,31 +31,6 @@ void PrintByLevel(TreeNode* root) {
     if (root == nullptr) 
         return;
     int level = 1;
-    queue<TreeNode*> current_level;
-    current_level.push(root);
-    queue<TreeNode*> next_level;
-    while (!next_level.empty() || (level == 1)) {
-        if(level > 1) 
-            swap(current_level, next_level);
-        while (!current_level.empty()) {
-            TreeNode* current_node = current_level.front();
-            cout << current_node->value << ' ' << level << '\n';
-            if (current_node->left != nullptr)
-                next_level.push(current_node->left);
-            if (current_node->right != nullptr)
-                next_level.push(current_node->right);
-            current_level.pop();
-        }
-        
-        level++;
-    }
-}
-
-int GetWidth(TreeNode* root) {
-    if (root == nullptr) {
-        return 0;
-    }
-    int level = 1;
     int max_width = 0;
     queue<TreeNode*> current_level;
     current_level.push(root);
@@ -67,25 +42,21 @@ int GetWidth(TreeNode* root) {
         while (!current_level.empty()) {
             TreeNode* current_node = current_level.front();
             current_width++;
-            if (current_node->left != nullptr)
-                next_level.push(current_node->left);
-            if (current_node->right != nullptr)
-                next_level.push(current_node->right);
+            cout << current_node->value << ' ' << level << '\n';
+            if (current_node->left != nullptr) next_level.push(current_node->left);
+            if (current_node->right != nullptr) next_level.push(current_node->right);
             current_level.pop();
         }
         max_width = max_width > current_width ? max_width : current_width;
         level++;
     }
-    return max_width;
+    cout << max_width << endl;
 }
 
 int main() {
-    //string test_sample[3] = {"ABF###CD#G##E##", "AB#D##CE###", "ABC##D#E###"};
+    string tree; cin >> tree;
     int i = 0;
-    string tree;
-    cin >> tree;
     TreeNode* root = CreateTree(tree, i);
     PrintByLevel(root);
-    cout << GetWidth(root);
     return 0;
 }
